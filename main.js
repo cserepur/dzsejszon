0856790d05fe54fc2015c5ef2813586d1ac2b6c4
cserepur <160589205+cserepur@users.noreply.github.com>
2025-04-10 10:35:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const usersData = document.getElementById('usersTable');
-
+// Űrlap elérése, eseményfigyelő hozzáadása
 document.getElementById('userForm').addEventListener('submit', (e) => {
+    //HTML űrlap alapértelmezett viselkedés megakadályozása
     e.preventDefault();
-
+    //FormData objektum létrehozása, az űrlap esemény hozzárendelése
+    //(e.target) az esemény címzettje
     const myformData = new FormData(e.target);
+    //Az űrlap adatok áthelyezése a userData objektumba
     const userData = Object.fromEntries(myformData);
-    console.log(userData);
-})
+
+
+    console.log(userData)
+    /*Másolatr készítése a(z) userData objektumról
+    const userObjekt = {
+      Vezetéknév: userData.vezeteknev,
+      Keresztnév: userData.keresztnev,
+      Email: userData.email,
+      Telefon: userData.telefonszam
+  
+      
+    }*/
+
+
+    console.log(userData)
+
+
+    const userDataJSON = JSON.stringify(userData, null, 2);
+    localStorage.setItem('userDataKey', userDataJSON);
+}) 
